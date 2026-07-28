e14c3b54f2d72949becf7d87cebd19f545a5c222
PugPapa0 <becktrav3@icloud.com>
2026-07-27 17:16:42 -0700

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+/********************************************
+*	AUTHOR:	<name>
+* COLLABORATORS: <names>
+*	COURSE:	CS 111 Intro to CS I - Java
+*	LAST MODIFIED:	<date>
+********************************************/
+
+public class Main 
+{
+  public static void main(String[] args)
+  {
+
+  }
+}
